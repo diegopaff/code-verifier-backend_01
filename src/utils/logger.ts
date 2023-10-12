a64cde0833c9c7f67bdfa23834166b744219a105
diegopaff <110573986+diegopaff@users.noreply.github.com>
2023-10-11 23:41:35 -0300

    a

---
+++ src/utils/logger.ts
@@ -0,0 +1,14 @@
+export const LogInfo = (message: string) => {
+  console.log(`Info: ${message}`)
+}
+export const LogSucces = (message: string) => {
+  console.log(`Succes: ${message}`)
+}
+
+export const LogError = (message: string) => {
+  console.log(`Error: ${message}`)
+}
+
+export const LogWarning = (message: string) => {
+  console.log(`Warning: ${message}`)
+}
